6d24d4c19cd25cfa9a91feb38317dca7aae8c90f
EMBEDDED <wway555@naver.com>
2023-11-16 16:53:36 +0900

    2023.11.16

---
+++ Intel_Edge/MissionRect/classes.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <iostream>
+#define ABS(x)  (((x)<0)?(-(x)):(x))
+#define MIN(x,y) (((x)<(y))?(x):(y))
+#define MAX(x,y) (((x)>(y))?(x):(y))
+class Point
+{
+private:
+public:
+	int x;
+	int y;
+
+	Point(int x1 = 0, int y1 = 0) : x(x1), y(y1)
+	{
+		std::cout << "Point 생성("<<x<<","<<y<<")\n";
+	}
+	~Point()
+	{
+		std::cout << "Point 소멸(" << x << "," << y << ")\n";
+	}
+
+	double Dist(Point p);		//Distance
+	void Show(const char* s = NULL);	//New Current point, s:Point name
+	void ShowEx(const char* s = NULL);	//New Current point, no Linefeed
+
+	Point& operator+(Point p);
+};
+
+class Rect
+{
+public:
+	Point p1, p2;
+	Point LL, LR, UL, UR;
+	int x1, x2, y1, y2;
+private:
+	void Init()		//생성자에서 호출되는 외부함수
+	{
+		LL = Point(MIN(p1.x, p2.x), MIN(p1.y, p2.y));
+		LR = Point(MAX(p1.x, p2.x), MIN(p1.y, p2.y));
+		UL = Point(MIN(p1.x, p2.x), MAX(p1.y, p2.y));
+		UR = Point(MAX(p1.x, p2.x), MAX(p1.y, p2.y));
+	}
+public:
+	Rect(Point pp1, Point pp2) : p1(pp1), p2(pp2),		// 생성자
+		x1(pp1.x), y1(pp1.y), x2(pp2.x), y2(pp2.y) 
+	{
+		Init();
+	}
+	Rect(int xx1 = 0, int yy1 = 0, int xx2 = 0, int yy2 = 0)
+	{
+		x1 = xx1; y1 = yy1;
+		x2 = xx2; y2 = yy2;
+		p1 = Point(xx1, yy1);
+		p2 = Point(xx2, yy2);
+		Init();
+	}
+	double Area();
+	Rect& Show(const char* s);		// RECT(P1(x1,y1), P2(x2,y2))
+	Rect& operator+(Rect r);
+	Rect& operator-(Rect r);
+	double operator*(Rect r);
+};
